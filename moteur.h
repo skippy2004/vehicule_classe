60d12f27d8497d2fd90ceebef0dba1f056ff65b8
skippy2004 <56gaming56@gmail.com>
2023-09-14 11:16:11 +0200

    moteur .h et .cpp, premiere version fini

--- moteur.h
+++ moteur.h
@@ -1,1 +1,57 @@
-#pragma once
+/*****************************************************************//**
+ * \file   mission.h
+ * \brief  Declaration de la classe entreprise
+ *
+ * \author Pierre
+ * \date   June 2023
+ *********************************************************************/
+
+#ifndef MISSION_H
+#define MISSION_H
+#include <iostream>
+#include <string>
+#include "Electrique.h"
+#include "Thermique.h"
+
+using namespace std;
+
+
+/*! \class Entreprise
+   * \brief classe representant une entreprise. 2 attributs prives
+   *
+   * Cette classe a une finalite pedagogique :
+   * \li : Rappel du C++
+   * \li : commentaire avec Doxygen
+   * \li : git, github
+   */
+class Moteur
+{
+
+private:
+    /**
+     * Membre prive puissance : puissance du moteur
+     */
+    int puissance;
+    
+   
+protected:
+
+
+
+public:
+    /*Classe sous forme canonique*/
+    Moteur();
+    ~Moteur();
+    Moteur(const Moteur& entre);
+    Moteur& operator = (const Moteur& entre);
+
+    Moteur(int puissance);
+
+    int getpuissance();
+    void setpuissance(int puissance);
+
+    
+
+};
+
+#endif
